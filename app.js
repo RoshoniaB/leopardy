1289bb4e6654724ad29701f3ad28af88b43dd5c0
RoeBrooks <roshoniabrooks@gmail.com>
2020-07-13 15:27:06 -0500

    finished grabing titles from api

--- app.js
+++ app.js
@@ -5,16 +5,36 @@ const square = document.querySelectorAll('.square');
 
 square.forEach(square =>{
     square.addEventListener('click', handleClick);
+
 });
 
-function switchTeam(){
-    team1 = team1 === 'X' ? 'O' : 'X';
-};
+// function switchTeam(){
+//     team1 = team1 === 'X' ? 'O' : 'X';
+// };
 
 function handleClick(event) {
-    event.target.innerHTML = team1;
-   console.log(event.target.id)
-  
+    fetch('http://www.jservice.io/api/categories?count=100')
+			.then((res) => {
+				return res.json();
+			})
+			.then((res) => {
+                console.log(res);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+    
 };
 
 const category = document.querySelectorAll('.category');
+
+function addOption(event) {
+	const moneybtn = document.querySelectorAll('.square');
+
+	const button = document.createElement('BUTTON');
+	const text = document.createTextNode('BUTTON');
+
+	button.appendChild(text);
+	moneybtn.appendChild(button);
+	console.log(event.target.div);
+}
