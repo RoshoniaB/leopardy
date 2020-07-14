ad6dfad03d4a7e3700d4f25c2948b0f9230cb8f3
RoeBrooks <roshoniabrooks@gmail.com>
2020-07-14 13:54:01 -0500

    randomized catigories

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-let team1 = 'X'
+
 
 const square = document.querySelectorAll('.square');
 
 
-square.forEach(square =>{
-    square.addEventListener('click', handleClick);
+// square.forEach(square =>{
+//     square.addEventListener('click', handleClick);
 
 
-});
+// });
         //   let button = document.createElement('button');
         //     let moneybtn = document.querySelectorAll('.square');
         //     console.log(moneybtn);
         //     moneybtn.appendChild(button);
         //     document.body.appendChild(button);
         //     button.innerHTML = 'hello';
-// function switchTeam(){
-//     team1 = team1 === 'X' ? 'O' : 'X';
-// };
+
 
 
 // filter out categories for the top/////////////////////////
@@ -47,20 +45,28 @@ square.forEach(square =>{
 ////////////////////////////////////////////////////////////
 
 
-      
-// randomizing the output is good but repeating the numbers is a problem. 
 
-function handleClick(event) {
-    fetch('http://www.jservice.io/api/clues?categories?count=100')
+const category = document.querySelectorAll('.category');
+category.forEach(category => {
+    category.addEventListener('click', handleClickCat )
+})
+    
+
+function handleClickCat(event) {
+    fetch('http://www.jservice.io/api/categories?count=100')
 			.then((res) => {
 				return res.json();
 			})
 			.then((res) => {
-                console.log(res[Math.floor(Math.random()*100)].category);
-
-
-
-            
+                ////////////Randomized the categories/////////////
+                ////////////dont know how to print out in boxes/////////
+                const max = 100
+                let userinput = 6
+                
+                for (let i=0; i<userinput; i++){
+                    // console.log(res[Math.floor(Math.random()*Math.floor(max))])
+                    category.innerText = res[Math.floor(Math.random()*Math.floor(max))];
+                }
 
 			})
 			.catch((error) => {
@@ -69,10 +75,27 @@ function handleClick(event) {
     
 };
 
-// const category = document.querySelectorAll('.category');
-// category.forEach(category => {
-//     category.addEventListener('click', )
-// })
+
+// randomizing the output is good but repeating the numbers is a problem. /////
+
+// function handleClick(event) {
+//     fetch('http://www.jservice.io/api/clues?categories?count=100')
+// 			.then((res) => {
+// 				return res.json();
+// 			})
+// 			.then((res) => {
+//                 console.log(res[Math.floor(Math.random()*Math.floor(max))].category);
+
+// 			})
+// 			.catch((error) => {
+// 				console.log(error);
+// 			});
+    
+// };
+
+
+//////////////////////////////////////////////////////////////
+
 
 
     
