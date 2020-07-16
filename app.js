7641d67802a39acf67a410a807b351931588e0f5
RoeBrooks <roshoniabrooks@gmail.com>
2020-07-16 09:35:32 -0500

    added score element

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 let startScore = 0
 
-const page = document.querySelector('div');
-page.addEventListener('click', createBtns)
+// const page = document.querySelector('div');
+// page.addEventListener('click', createBtns)
 
-let scoreEle = document.querySelectorAll('.score')
-scoreEle.textContent = startScore
+// let scoreEle = document.querySelectorAll('.score')
+// scoreEle.textContent = startScore
 
-//////////////////cant add a button to each div///////////////////
+// //////////////////cant add a button to each div///////////////////
 
-function createBtns() {
+// function createBtns() {
     
-// 	let yesBtn = document.createElement('button');
-//     let noBtn = document.createElement('button');
-//     yesBtn.className = 'choices'
-//     noBtn.className = 'choices'
-//     // yesBtn.style='display:none;'
-//     // noBtn.style='display:none;'
-// 	yesBtn.appendChild(document.createTextNode('Yes!'));
-//     noBtn.appendChild(document.createTextNode('No...'));
-// 		page.appendChild(yesBtn);
-//             page.appendChild(noBtn);	
-            scoreEle.textContent = ++startScore
-//    for(let i=0; i<page;i++){
-//         console.log(page[i])
-//        list = updatediv(page[i].innerHTML);
-//        page[i].innerHTML = list
-//     }   
-}    
+// // 	let yesBtn = document.createElement('button');
+// //     let noBtn = document.createElement('button');
+// //     yesBtn.className = 'choices'
+// //     noBtn.className = 'choices'
+// //     // yesBtn.style='display:none;'
+// //     // noBtn.style='display:none;'
+// // 	yesBtn.appendChild(document.createTextNode('Yes!'));
+// //     noBtn.appendChild(document.createTextNode('No...'));
+// // 		page.appendChild(yesBtn);
+// //             page.appendChild(noBtn);	
+//             scoreEle.textContent = ++startScore
+// //    for(let i=0; i<page;i++){
+// //         console.log(page[i])
+// //        list = updatediv(page[i].innerHTML);
+// //        page[i].innerHTML = list
+// //     }   
+// }    
 
-createBtns()
+// createBtns()
+
+const square = document.querySelectorAll('.square')
+const oudometer = document.querySelector
