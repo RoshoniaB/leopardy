5625f1f57c37b8fa443d1899d71a6767f2cbf478
RoeBrooks <roshoniabrooks@gmail.com>
2020-07-15 21:48:08 -0500

    separated the text in questions.html and repositioned it

--- questions.js
+++ questions.js
@@ -1,23 +1,21 @@
 /******/ ///////////randomized questions for each value////////////////****** */
-const max = 1; 
+const max = 100; 
 // function myfun(){
 // 	document.querySelector('.gameboard').style.display = 'block'
 // }
 
 
 
-const square1 = document.querySelectorAll('.gameboard')
-square1.forEach((square1) => {
-	square1.addEventListener('click', handleClickQues);
-});
+// const square1 = document.querySelectorAll('.gameboard')
+// square1.forEach((square1) => {
+// 	square1.addEventListener('click', handleClickQues);
+// });
 function handleClickQues(event) {
 	fetch(`http://www.jservice.io/api/clues?value=100`)
 		.then((res) => {
 			return res.json();
 		})
 		.then((res) => {
-			////////////////////////cant figure out how to target answer//////////
-			////////////////////and print out in boxes////////////////
 			let randomindex = Math.floor(Math.random() * Math.floor(max))
 			let square1 = res[randomindex].question;
 			let questionParagraph = document.createElement('p');
@@ -27,10 +25,13 @@ function handleClickQues(event) {
 			questionParagraph.innerText = square1
 			document.querySelector('.gameboard').appendChild (questionParagraph)
 			document.querySelector('.gameboard').appendChild(answerParagraph);
-			answerParagraph.classList.add('hide')
+			questionParagraph.classList.add('quest')
+			answerParagraph.classList.add('hide','ans')
+
 			let showAnswer = document.querySelector('.show-answer')
-            showAnswer.addEventListener('click', function(){
-				answerParagraph.classList.remove('hide1')
+			
+			showAnswer.addEventListener('click', ()=>{
+				answerParagraph.classList.remove('hide')
 			})
 			
 		})
@@ -38,73 +39,137 @@ function handleClickQues(event) {
 			console.log(error);
 		});
 }
-
+handleClickQues()
 
 ///////////////////////////////////////////////////////////////////////////////
 // const square2 = document.querySelectorAll('.squ2');
 // square2.forEach((square2) => {
 // 	square2.addEventListener('click', handleClickQues);
 // });
-// function handleClickQues(event) {
-// 	fetch(`http://www.jservice.io/api/clues?value=200`)
-// 		.then((res) => {
-// 			return res.json();
-// 		})
-// 		.then((res) => {
-// 			console.log(res[Math.floor(Math.random() * Math.floor(max))].question);
-// 		})
-// 		.catch((error) => {
-// 			console.log(error);
-// 		});
-// }
+function handleClickQues(event) {
+	fetch(`http://www.jservice.io/api/clues?value=200`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+				let randomindex = Math.floor(Math.random() * Math.floor(max));
+				let square1 = res[randomindex].question;
+				let questionParagraph = document.createElement('p');
+				let answerParagraph = document.createElement('p');
+				let answer = res[randomindex].answer;
+				answerParagraph.innerText = answer;
+				questionParagraph.innerText = square1;
+				document.querySelector('.gameboard').appendChild(questionParagraph);
+				document.querySelector('.gameboard').appendChild(answerParagraph);
+				questionParagraph.classList.add('quest');
+				answerParagraph.classList.add('hide', 'ans');
+
+				let showAnswer = document.querySelector('.show-answer');
+
+				showAnswer.addEventListener('click', () => {
+					answerParagraph.classList.remove('hide');
+				});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
 // ///////////////////////////////////////////////////////////////////////////////
 // const square3 = document.querySelectorAll('.squ3');
 // square3.forEach((square3) => {
 // 	square3.addEventListener('click', handleClickQues);
 // });
-// function handleClickQues(event) {
-// 	fetch(`http://www.jservice.io/api/clues?value=300`)
-// 		.then((res) => {
-// 			return res.json();
-// 		})
-// 		.then((res) => {
-// 			console.log(res[Math.floor(Math.random() * Math.floor(max))].question);
-// 		})
-// 		.catch((error) => {
-// 			console.log(error);
-// 		});
-// }
+function handleClickQues(event) {
+	fetch(`http://www.jservice.io/api/clues?value=300`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			let randomindex = Math.floor(Math.random() * Math.floor(max));
+			let square1 = res[randomindex].question;
+			let questionParagraph = document.createElement('p');
+			let answerParagraph = document.createElement('p');
+			let answer = res[randomindex].answer;
+			answerParagraph.innerText = answer;
+			questionParagraph.innerText = square1;
+			document.querySelector('.gameboard').appendChild(questionParagraph);
+			document.querySelector('.gameboard').appendChild(answerParagraph);
+			questionParagraph.classList.add('quest');
+			answerParagraph.classList.add('hide', 'ans');
+
+			let showAnswer = document.querySelector('.show-answer');
+
+			showAnswer.addEventListener('click', () => {
+				answerParagraph.classList.remove('hide');
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
 // //////////////////////////////////////////////////////////////////////////////
 // const square4 = document.querySelectorAll('.squ4');
 // square4.forEach((square4) => {
 // 	square4.addEventListener('click', handleClickQues);
 // });
-// function handleClickQues(event) {
-// 	fetch(`http://www.jservice.io/api/clues?value=400`)
-// 		.then((res) => {
-// 			return res.json();
-// 		})
-// 		.then((res) => {
-// 			console.log(res[Math.floor(Math.random() * Math.floor(max))].question);
-// 		})
-// 		.catch((error) => {
-// 			console.log(error);
-// 		});
-// }
+function handleClickQues(event) {
+	fetch(`http://www.jservice.io/api/clues?value=400`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			let randomindex = Math.floor(Math.random() * Math.floor(max));
+			let square1 = res[randomindex].question;
+			let questionParagraph = document.createElement('p');
+			let answerParagraph = document.createElement('p');
+			let answer = res[randomindex].answer;
+			answerParagraph.innerText = answer;
+			questionParagraph.innerText = square1;
+			document.querySelector('.gameboard').appendChild(questionParagraph);
+			document.querySelector('.gameboard').appendChild(answerParagraph);
+			questionParagraph.classList.add('quest');
+			answerParagraph.classList.add('hide', 'ans');
+
+			let showAnswer = document.querySelector('.show-answer');
+
+			showAnswer.addEventListener('click', () => {
+				answerParagraph.classList.remove('hide');
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
 // ////////////////////////////////////////////////////////////////////////////
-// const square5 = document.querySelectorAll('.squ5');
-// square5.forEach((square5) => {
-// 	square5.addEventListener('click', handleClickQues);
-// });
-// function handleClickQues(event) {
-// 	fetch(`http://www.jservice.io/api/clues?value=500`)
-// 		.then((res) => {
-// 			return res.json();
-// 		})
-// 		.then((res) => {
-// 			console.log(res[Math.floor(Math.random() * Math.floor(max))].question);
-// 		})
-// 		.catch((error) => {
-// 			console.log(error);
-// 		});
-// }
+const square5 = document.querySelectorAll('.squ5');
+square5.forEach((square5) => {
+	square5.addEventListener('click', handleClickQues);
+});
+function handleClickQues(event) {
+	fetch(`http://www.jservice.io/api/clues?value=500`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			let randomindex = Math.floor(Math.random() * Math.floor(max));
+			let square1 = res[randomindex].question;
+			let questionParagraph = document.createElement('p');
+			let answerParagraph = document.createElement('p');
+			let answer = res[randomindex].answer;
+			answerParagraph.innerText = answer;
+			questionParagraph.innerText = square1;
+			document.querySelector('.gameboard').appendChild(questionParagraph);
+			document.querySelector('.gameboard').appendChild(answerParagraph);
+			questionParagraph.classList.add('quest');
+			answerParagraph.classList.add('hide', 'ans');
+
+			let showAnswer = document.querySelector('.show-answer');
+
+			showAnswer.addEventListener('click', () => {
+				answerParagraph.classList.remove('hide');
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
