f42ba9955498be0cbf45ad8707d3a899e4551c22
roshoniab <roshoniabrooks@gmail.com>
2020-09-26 19:11:31 -0500

    made changes to style.css

--- Home/home.js
+++ Home/home.js
@@ -8,11 +8,12 @@ square.addEventListener('click', handleIWon);
 function handleIWon(event) {
     const increment = parseInt(event.target.dataset.increment);
     if (event.target.classList.contains('btn')) {
-			console.log(event.target);
+			// console.log(event.target);
 			startScore += increment;
 			score.innerText = startScore;
-			if (startScore <= 1000) {
-				document.body.classList.add('game-over');
+			if (startScore >= 1000) {
+				// document.body.classList.add('game-over');
+				
 			}
 		}
 }
